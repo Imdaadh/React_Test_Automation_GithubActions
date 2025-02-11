c2c6ac8d0b9a3c8672b077ae2fc1a15933ae4415
Imdaad <imdaadj211@gmail.com>
2025-02-11 18:10:00 +0530

    changed the text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
       <p className='text-xl font-montserrat text-coral-red'>
           Welcome to Imdaad's CI/CD Integrated App with 
-          <span className='text-coral-red inline-block mt-3'> Cypress Automation Iresh</span>
+          <span className='text-coral-red inline-block mt-3'> Cypress Automation</span>
         </p>
 
         <p className='text-xl font-montserrat text-coral-red'>
